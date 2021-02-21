b8a893804bf7bcd53160486e394f41d5fbc8e5d8
merarli <service@mera.tokyo>
2021-02-21 17:58:30 +0900

    first commit

---
+++ docs/precache-manifest.b90ec36c94b9d1893f401656874fe0dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a942cf1c194e21ab3e43",
+    "url": "css/app.4d08c71d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1bf42990bedba6fb1fceec81ed32fe1a",
+    "url": "index.html"
+  },
+  {
+    "revision": "68e04d446471f671f9ed",
+    "url": "js/about.8eaab203.js"
+  },
+  {
+    "revision": "a942cf1c194e21ab3e43",
+    "url": "js/app.be2b2d18.js"
+  },
+  {
+    "revision": "a71e62ed12b5da23b4b5",
+    "url": "js/chunk-vendors.6bca2ecc.js"
+  },
+  {
+    "revision": "7482e20d65433deef570ccc86c34d453",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
